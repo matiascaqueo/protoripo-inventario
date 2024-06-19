046a6e93f7316e2f8f6c59407a7679c7333ea827
matias.caqueo <138621787+matiascaqueo@users.noreply.github.com>
2024-06-18 20:18:49 -0400

    Add files via upload

---
+++ prototipo/WindowsFormsApp1/Agregar.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Agregar : Form
+    {
+        public Agregar()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Agregar_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+           
+            string id = txtid.Text;
+            string nombreProducto = txtnombre.Text;
+            string descripcion = txtdescrpicion.Text;
+            string precio = txtprecio.Text;
+            string stock = txtstock.Text;
+            string categoria = txtcategoria.Text;
+
+           
+            string query = "INSERT INTO dbo.producto (id_Producto, Nombre, Descripcion, Precio, Stock, Categoria) VALUES (@id_Producto, @Nombre, @Descripcion, @Precio, @Stock, @Categoria)";
+
+            try
+            {
+                
+                using (SqlConnection conexion = new SqlConnection("server=DESKTOP-I41RIDO\\SQLEXPRESS;database=Tienda;integrated security=true"))
+                {
+                    conexion.Open();
+
+                    
+                    using (SqlCommand comando = new SqlCommand(query, conexion))
+                    {
+                        
+                        comando.Parameters.AddWithValue("@id_Producto", id);
+                        comando.Parameters.AddWithValue("@Nombre", nombreProducto);
+                        comando.Parameters.AddWithValue("@Descripcion", descripcion);
+                        comando.Parameters.AddWithValue("@Precio", precio);
+                        comando.Parameters.AddWithValue("@Stock", stock);
+                        comando.Parameters.AddWithValue("@Categoria",categoria);
+
+                     
+                        int result = comando.ExecuteNonQuery();
+                        if (result == 0)
+                        {
+                            MessageBox.Show("No pudo insertarse el registro");
+                        }
+                        else
+                        {
+                            MessageBox.Show("El registro se insertó exitosamente.");
+                        }
+                    }
+                }
+
+               
+                txtid.Text = "";
+                txtnombre.Text = "";
+                txtdescrpicion.Text = "";
+                txtprecio.Text = "";
+                txtstock.Text = "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
+
+
---
+++ prototipo/WindowsFormsApp1/Eliminar.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Eliminar : Form
+    {
+        public Eliminar()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtid_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            {
+                
+                string id = txtEliminar.Text;
+
+                
+                string query = "DELETE FROM dbo.Producto WHERE id_Producto = @id_Producto";
+
+                try
+                {
+                    
+                    using (SqlConnection conexion = new SqlConnection("server=DESKTOP-I41RIDO\\SQLEXPRESS;database=Tienda;integrated security=true"))
+                    {
+                        conexion.Open();
+
+                       
+                        using (SqlCommand comando = new SqlCommand(query, conexion))
+                        {
+                            
+                            comando.Parameters.AddWithValue("@id_Producto", id);
+
+                           
+                            int result = comando.ExecuteNonQuery();
+                            if (result == 0)
+                            {
+                                MessageBox.Show("No se encontró ningún producto con ese ID.");
+                            }
+                            else
+                            {
+                                MessageBox.Show("El producto se eliminó exitosamente.");
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al intentar eliminar el producto: " + ex.Message);
+                }
+                finally
+                {
+                    
+                    txtEliminar.Text = "";
+                }
+            }
+
+        }
+    }
+}
